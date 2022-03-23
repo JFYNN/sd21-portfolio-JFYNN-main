3d0aa1b37412c5ed1b6529f6012fe246dcc9e609
JFYNN <302425541@student.rocmondriaan.nl>
2022-03-23 13:57:37 +0100

    first commit

---
+++ js/quote.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('btn')
+let output = document.getElementById('output')
+let quote =[
+    The journey of a thousand miles begins with one step.
+
+Lao Tzu
+That which does not kill us makes us stronger.
+
+Friedrich Nietzsche
+]
